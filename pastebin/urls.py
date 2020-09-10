3d1dc457b15ef35f91ec06cdf069eca1714e558b
rajendra-a <rajendrareddy.akkala@gmail.com>
2020-09-10 16:23:15 +0530

    adding final commit to pastebin

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -5,8 +5,7 @@ urlpatterns = [
     path('', PasteCreate.as_view(),name="create"),
     path('pastes/', PasteList.as_view(), name='pastebin_paste_list'),
     path('paste/<int:pk>', PasteDetail.as_view(), name="pastebin_paste_detail"),
-    path('paste/delete/<int:pk>', PasteDelete.as_view(), name="pastebin_paste_update"),
-    path('paste/edit/<int:pk>', PasteUpdate.as_view(), name="pastebin_paste_delete")
-
+    path('paste/delete/<int:pk>', PasteDelete.as_view(), name="pastebin_paste_delete"),
+    path('paste/edit/<int:pk>', PasteUpdate.as_view(), name="pastebin_paste_update")
 
 ]
